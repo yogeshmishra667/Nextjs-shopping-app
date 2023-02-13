1eb0298a78e2f32b26fbd7958d1d05063a00b745
yogesh mishra <yogeshmishra667@gmail.com>
2023-02-13 20:13:05 +0530

    protect route

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,38 @@
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
 import { SessionProvider } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <StoreProvider>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </StoreProvider>
     </SessionProvider>
   );
 }
+
+const Auth = ({ children }) => {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=login required');
+    },
+  });
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+};
 
 export default MyApp;
